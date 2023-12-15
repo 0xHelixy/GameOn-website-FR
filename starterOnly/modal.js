1529efad4788c74af659598bce766bec2116aba7
Helixy <helixy@proton.me>
2023-12-15 17:43:46 +0100

    close modal, form completion & error messages

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,9 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close")
+
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -21,3 +24,73 @@ function launchModal() {
 }
 
 
+
+// function to hide the modal
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+// selecting close button in the modal
+const closeModalBtn = document.querySelector(".close");
+
+// add event listener to close button
+closeModalBtn.addEventListener("click", closeModal);
+
+
+
+
+function validate() {
+  console.log("testValidateform")
+  let isValid = true;
+
+  // clean error messages
+  document.querySelectorAll('.error-message').forEach(e => e.textContent = '');
+
+  // validate name
+  const firstName = document.getElementById('first').value;
+  if (firstName.length < 2) {
+    document.getElementById('error-first').textContent = 'Veuillez entrer 2 caractères ou plus pour le champ du prénom.';
+    isValid = false;
+  }
+
+  // validate lastname
+  const lastName = document.getElementById('last').value;
+  if (lastName.length < 2) {
+    document.getElementById('error-last').textContent = 'Veuillez entrer 2 caractères ou plus pour le champ du nom.';
+    isValid = false;
+  }
+
+  // validate email
+  const email = document.getElementById('email').value;
+  if (!email.match(/^\S+@\S+\.\S+$/)) {
+    document.getElementById('error-email').textContent = 'Vous devez entrer une adresse e-mail valide.';
+    isValid = false;
+  }
+
+  // validate number tournois
+  const quantity = document.getElementById('quantity').value;
+  if (isNaN(quantity) || quantity < 0 || quantity === '') {
+    document.getElementById('error-quantity').textContent = 'Veuillez entrer un nombre valide.';
+    isValid = false;
+  }
+
+  // validate select tournois
+  const locationChecked = document.querySelector('input[name="location"]:checked');
+  if (!locationChecked) {
+    document.getElementById('error-location').textContent = 'Vous devez choisir une option.';
+    isValid = false;
+  }
+
+  // validate general conditions
+  const termsChecked = document.getElementById('checkbox1').checked;
+  if (!termsChecked) {
+    document.getElementById('error-terms').textContent = 'Vous devez vérifier que vous acceptez les termes et conditions.';
+    isValid = false;
+  }
+
+  if (isValid) {
+    localStorage.removeItem('formData'); // Nettoyer localStorage
+  }
+
+  return isValid;
+}
